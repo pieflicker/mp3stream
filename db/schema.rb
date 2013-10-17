35ac2c3e7028f00c7fe5a26e87d1eda46eecd647
pieflicker <nesbitt.greg@gmail.com>
2013-10-17 17:00:12 -0400

    dropping searches table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017200530) do
-
-  create_table "searches", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20131017205901) do
 
   create_table "song_uploader", force: true do |t|
   end
